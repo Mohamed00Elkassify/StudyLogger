06be8d096389b245650d6352f88c2175884a6628
Mohamed00Elkassify <mohamed0elkassify@gmail.com>
2025-07-30 01:32:40 +0300

    register view

--- study_sessions/views.py
+++ study_sessions/views.py
@@ -1,3 +1,19 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import (ListView, CreateView, UpdateView, DeleteView, FormView)
+from django.contrib.auth import login
+from django.urls import reverse_lazy
+from .models import Sessions
+from .forms import RegisterForm, SessionsForm
+
 
 # Create your views here.
+class RegisterView(FormView):
+    form_class = RegisterForm
+    template_name = 'registration/signup'
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return super().form_valid(form)
